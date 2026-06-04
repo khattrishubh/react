63a860905ac6d88cb5102f14f4e419a57d1f03f2
“khattrishubh” <khattrishubh29@gmail.com>
2026-06-04 11:35:47 +0530

    props react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import Button  from './Button.jsx';
+/*import Button  from './Button.jsx';
 
 function App() 
 {
   return (<Button />);
+}
+
+export default App;
+*/
+
+
+import Student from './Student.jsx';
+
+function App() 
+{
+  return (
+  <>
+    <Student name="Shubh" age={20} isStudent={true} />
+  </>);
+
 }
 
 export default App;
